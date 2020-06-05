7e658c4d590d529bbbbd49a3134f9dc5e68c8309
WLSF <willianluigii@hotmail.com>
2020-06-05 18:24:33 -0300

    :fire: Remove indications factory

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -9,10 +9,6 @@ defmodule Bank.Factory do
     %Account{birth_date: ~D[2010-04-17], city: "some city", country: "some country", cpf: "123.456.789-00", email: "some@email.com", gender: "some gender", name: "some name", state: "some state"}
   end
 
-  def build(:indication) do
-    #%Indication{sender} still thinking about it
-  end
-
   def build(factory, attrs \\ []), do: factory |> build() |> struct(attrs)
   def insert!(factory, attrs \\ []), do: factory |> build(attrs) |> Repo.insert!()
 end
